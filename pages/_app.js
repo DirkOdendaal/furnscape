56cf2f81333e579c021e5f81636a3c70b6ec5c6e
Dirk Odendaal <odendaaldirkjnr@gmail.com>
2023-05-09 17:15:24 +0200

    Menu And Cart Changes + Favicon

--- pages/_app.js
+++ pages/_app.js
@@ -4,17 +4,23 @@ import { Layout } from "../components";
 import { AuthContextProvider } from "../context/AuthContext";
 import { StateContext } from "../context/StateContext";
 import { Toaster } from "react-hot-toast";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <StateContext>
-      <AuthContextProvider>
-        <Layout>
-          <Toaster position="bottom-right" reverseOrder={false} />
-          <Component {...pageProps} />
-        </Layout>
-      </AuthContextProvider>
-    </StateContext>
+    <>
+      <Head>
+        <link rel="shortcut icon" href="./favicon.ico" />
+      </Head>
+      <StateContext>
+        <AuthContextProvider>
+          <Layout>
+            <Toaster position="bottom-right" reverseOrder={false} />
+            <Component {...pageProps} />
+          </Layout>
+        </AuthContextProvider>
+      </StateContext>
+    </>
   );
 }
 
